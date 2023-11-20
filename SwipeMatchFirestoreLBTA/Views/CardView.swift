df71dda26ded0ece9252e1979f4f9f5374b7c1c8
meilynwong <90463529+meilynwong@users.noreply.github.com>
2023-11-19 17:56:13 -0700

    done lesson 4

--- SwipeMatchFirestoreLBTA/Views/CardView.swift
+++ SwipeMatchFirestoreLBTA/Views/CardView.swift
@@ -10,17 +10,47 @@ import UIKit
 
 class CardView: UIView {
     
-    
     fileprivate let imageView =  UIImageView(image: UIImage(named: "profilePic"))
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        backgroundColor = .red
+        layer.cornerRadius = 10
+        clipsToBounds = true
         
         addSubview(imageView)
         imageView.fillSuperview()
+        let pangesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(pangesture)
     }
     
+    
+    
+    
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case.ended:
+            handleEnded()
+        default:
+            ()
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+    
+    fileprivate func handleEnded() {
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            self.transform = .identity
+        }) { (_) in
+            
+        }
+    }
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
